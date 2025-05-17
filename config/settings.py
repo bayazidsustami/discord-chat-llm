1978663bf18731401eaa57aae636ca8721b04fc0
bayazidsustami <bayazidsustamy@gmail.com>
2025-05-18 01:12:56 +0700

    fix : remove guild id

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD_ID = int(os.getenv("GUILD_ID"))
 
 BEDROCK_ACCESS_KEY_ID = os.getenv("BEDROCK_ACCESS_KEY_ID")
 BEDROCK_SECRET_ACCESS_KEY = os.getenv("BEDROCK_SECRET_ACCESS_KEY")
